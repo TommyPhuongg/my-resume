3b8b6c62149f75835f670907daa2c6741fc8f5e1
Tommy Phuong  <tphuong4@my.bcit.ca>
2025-11-05 22:21:35 -0800

    made resume site and build file

---
+++ script.js
@@ -0,0 +1,12 @@
+// Simple scroll behavior for smooth navigation
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', e => {
+    e.preventDefault();
+    document.querySelector(anchor.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+// Optional console message
+console.log("Welcome to Tommy Phuong’s Resume Website!");
